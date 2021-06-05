863383a09b40686ba61152c0177c509284b43074
yoshito <yoshito5211@gmail.com>
2021-06-06 03:31:11 +0900

    編集機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,16 +16,19 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    task = Task.find(params[:id])
+    task.update(task_params)
   end
 
   def destroy
   end
 
   private
-  
+
   def task_params
     params.require(:task).permit(:title, :content)
   end
